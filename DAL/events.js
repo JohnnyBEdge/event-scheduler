38cf4537c87e248b6a9e55605a7119d35f6ebc70
John Martin <jrmartin182@gmail.com>
2020-05-18 09:08:44 -0600

    created GET method for API

--- DAL/events.js
+++ DAL/events.js
@@ -5,10 +5,36 @@ const ObjectID = require('mongodb').ObjectID;
 const url = process.env.DB_URL;
 
 // Database name
-const dbName = "event-scheduler";
+const dbName = "event_scheduler";
 const collName = "events";
 
 // Database settings
-const settings = { useUnifiedTechnology: true};
+const settings = { useUnifiedTopology: true };
 
+const getEvents = () => {
+    const promise = new Promise((resolve, reject) => {
+        MongoClient.connect(url, settings, function(err, client){
+            if(err){
+                reject(err);
+            } else {
+                console.log(`Successfully connected to DB: ${dbName} for GET.`);
+                const db = client.db(dbName);
+                const collection = db.collection(collName);
+                collection.find({}).toArray(function(err,docs){
+                    if(err){
+                        reject(err);
+                    } else {
+                        console.log(docs);
+                        resolve(docs);
+                        client.close();
+                    };
+                })
+            };
+        })
+    })
+    return promise;
+}
 
+module.exports = {
+    getEvents
+}
--- routes/api/events.js
+++ routes/api/events.js
@@ -1,9 +1,18 @@
 const express = require('express');
 const router = express.Router();
+const {
+  getEvents
+} = require('../../DAL/events');
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+router.get('/', async function(req, res, next) {
+  try{
+    const data = await getEvents();
+    res.send(data);
+  } catch(err){
+    console.log(err);
+    res.send(500, 'Internal server error; check logs');
+  };
 });
 
 module.exports = router;
