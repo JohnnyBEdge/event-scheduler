eb16e3b6f84303e77606dfe4fda9d22032b2b0b4
John Martin <jrmartin182@gmail.com>
2020-06-18 16:10:45 -0600

    adding ui

---
+++ public/precache-manifest.6acbfcb06f7c583487f2950492a72225.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ec2a47ccf8ec6e0df8170d66a789c75b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "4ab2665d9f23b407ffca",
+    "url": "/static/css/main.56a849fa.chunk.css"
+  },
+  {
+    "revision": "d30ae27435bca97ae941",
+    "url": "/static/js/2.0947dbb2.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/static/js/2.0947dbb2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4ab2665d9f23b407ffca",
+    "url": "/static/js/main.51bacd73.chunk.js"
+  },
+  {
+    "revision": "205b406daa6de481ab76",
+    "url": "/static/js/runtime-main.526b4ad0.js"
+  }
+]);
